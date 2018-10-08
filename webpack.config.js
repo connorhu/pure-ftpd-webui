3fd79a3f23b36f84a2adf326b17a0ec5588eb0ed
Gossler Karoly <connor@connor.hu>
2018-10-08 10:51:58 +0200

    versioning is always on

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    .enableVersioning()
 
     // enables Sass/SCSS support
     .enableSassLoader()
